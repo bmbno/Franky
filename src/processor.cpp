c9e4cdb1378eed3c53c4435143580a23e853556e
Gabe Ng <gng10836@gmail.com>
2021-09-10 00:24:14 -0400

    deconstructor method

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,14 +1,11 @@
-#include<processor.h>
-Processor::Processor(int memory_size=4000, int num_registers=3){
+#include <processor.h>
+Processor::Processor(int memory_size = 4000, int num_registers = 3)
+{
     _memory = new int(memory_size);
-    _register = new int(num_registers)
-
-    ~Processor(){
-        delete _memory;
-        delete _register;
-    }
-
+    _register = new int(num_registers);
 }
-
-
-    
+Processor::~Processor()
+{
+    delete _memory;
+    delete _register;
+}
